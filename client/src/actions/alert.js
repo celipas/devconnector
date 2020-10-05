9bea28e80c141aa54b9da24941bec3d3a4099792
celipas <celipas@gmail.com>
2020-10-04 22:44:12 -0600

    Alert compontent, set alert and remove alert actions

--- client/src/actions/alert.js
+++ client/src/actions/alert.js
@@ -4,10 +4,15 @@ export const setAlert = (msg, alertType) => dispatch => {
   const id = uuid.v4();
   dispatch({
     type: SET_ALERT,
-    payload: {
-      msg,
-      alertType,
-      id,
-    },
+    payload: { msg, alertType, id },
   });
+
+  setTimeout(
+    () =>
+      dispatch({
+        type: REMOVE_ALERT,
+        payload: id,
+      }),
+    5000
+  );
 };
--- client/src/reducers/alert.js
+++ client/src/reducers/alert.js
@@ -2,7 +2,7 @@ import { SET_ALERT, REMOVE_ALERT } from '../actions/types';
 
 const initialState = [];
 
-const alert = (state = initialState, action) => {
+export default function(state = initialState, action) {
   const { type, payload } = action;
   switch (type) {
     case SET_ALERT:
@@ -12,6 +12,4 @@ const alert = (state = initialState, action) => {
     default:
       return state;
   }
-};
-
-export default alert;
+}
