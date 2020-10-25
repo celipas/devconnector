5e0725c4af3f4a44d3dcb668d41c7fbde1369e3b
celipas <celipas@gmail.com>
2020-10-25 16:47:55 -0600

    prepared for deployment git push origin master

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const connectDB = require('./config/db');
 const http = require('http');
+const path = require('path');
 
 const app = express();
 
@@ -9,7 +10,7 @@ connectDB();
 
 // Init Middleware
 app.use(express.json({ extended: false }));
-app.get('/', (req, res) => res.send('API Running'));
+// app.get('/', (req, res) => res.send('API Running'));
 
 // Define Routes
 app.use('/api/users', require('./routes/api/users'));
@@ -18,14 +19,14 @@ app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
 
 // // Serve static assets in product
-// if (process.env.NODE_ENV === 'production') {
-//   //Set static folder
-//   app.use(express.static('client/build'));
+if (process.env.NODE_ENV === 'production') {
+  //   //Set static folder
+  app.use(express.static('client/build'));
 
-//   app.get('*', (req, res) => {
-//     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-//   });
-// }
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
